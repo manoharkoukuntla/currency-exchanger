36ec86d4ad1831d6e4622a4d3165ddf27974d872
manohark <manohark@spiceworks.com>
2021-02-25 01:17:31 +0530

    change app name in swagger ui

---
+++ src/main/java/com/nosto/exchanger/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.nosto.exchanger.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Currency exchange converter API",
+                version = "v1",
+                description = "This app provides REST APIs for converting currencies. " +
+                        "App uses <a>https://api.exchangeratesapi.io</a> as source of exchange values.",
+                contact = @Contact(
+                        name = "Manohar",
+                        email = "manoharkoukuntla3@gmail.com"
+                )
+        )
+)
+public class SwaggerConfig {
+}
