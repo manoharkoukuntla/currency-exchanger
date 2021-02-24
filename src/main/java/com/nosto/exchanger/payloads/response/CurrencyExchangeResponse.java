68ec830091848ab34c909c7b786d2da1d6dabcf4
manohark <manohark@spiceworks.com>
2021-02-24 20:48:51 +0530

    remove deprecation warings

--- src/main/java/com/nosto/exchanger/payloads/response/CurrencyExchangeResponse.java
+++ src/main/java/com/nosto/exchanger/payloads/response/CurrencyExchangeResponse.java
@@ -2,10 +2,12 @@ package com.nosto.exchanger.payloads.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Collections;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 @AllArgsConstructor
 public class CurrencyExchangeResponse extends BaseResponse {
     public CurrencyExchangeResponse(){
