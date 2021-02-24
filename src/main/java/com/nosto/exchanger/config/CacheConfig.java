53bd721cdd9fd3cde0e502dbe691f83b2bb76db7
manohark <manohark@spiceworks.com>
2021-02-24 11:24:57 +0530

    update redis config for production

--- src/main/java/com/nosto/exchanger/config/CacheConfig.java
+++ src/main/java/com/nosto/exchanger/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.nosto.exchanger.config;
 
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
@@ -7,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -17,8 +17,12 @@ public class CacheConfig {
     public static final String EXCHANGE_RATES = "EXCHANGE_RATES";
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
     }
 
     @Bean
