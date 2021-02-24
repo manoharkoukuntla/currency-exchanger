cedb7850648a1b04269932c83aabba9dc811b74f
manohark <manohark@spiceworks.com>
2021-02-24 13:10:17 +0530

    add redis variables

--- src/main/java/com/nosto/exchanger/config/CacheConfig.java
+++ src/main/java/com/nosto/exchanger/config/CacheConfig.java
@@ -1,17 +1,10 @@
 package com.nosto.exchanger.config;
 
-import io.lettuce.core.RedisClient;
-import io.lettuce.core.RedisURI;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,19 +14,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class CacheConfig {
     public static final String EXCHANGE_RATES = "EXCHANGE_RATES";
 
+    @Autowired
+    private RedisConfiguration redisConfiguration;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisConfiguration);
     }
-
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager(EXCHANGE_RATES);
-        return RedisCacheManager
-                .builder(redisConnectionFactory).build();
-    }
-
 
     @CacheEvict(allEntries = true, value = {EXCHANGE_RATES})
     @Scheduled(fixedDelay = 60 * 60 * 1000)
